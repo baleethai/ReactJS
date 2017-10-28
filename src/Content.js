12d05c49d24b255ed4ed00e43ecc97ee3c423d7e
Thinnakorn Pattha <thinnakorn.pattha@itorama.com>
2017-10-28 10:06:25 +0700

    state

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 
 class Content extends Component {
+	constructor() {
+		super()
+		this.state = {
+			name: "Unlike",
+			count: 0
+		}
+	}
 	render() {
+		setTimeout( () => {
+			this.setState({
+				name: "Like"
+			})
+		}, 2000)
+		setInterval( () => {
+			this.setState({count: this.state.count+1})
+		}, 1000)
 		return(
 			<div>
 				<h3>{ this.props.title }</h3>
+				<p>Status : {this.state.name} {this.state.count}</p>
 			</div>
 		)
 	}
